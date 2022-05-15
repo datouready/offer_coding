44791c2b35ba8145a68b4b63caa3ef8a2751e7fc
Ready_liu <425577456@qq.com>
2022-05-15 08:17:14 +0800

    Create 剑指 Offer 63. 股票的最大利润.cpp

--- 剑指 Offer 63. 股票的最大利润.cpp
+++ 剑指 Offer 63. 股票的最大利润.cpp
@@ -1,5 +1,41 @@
-//�����ĳ��Ʊ�ļ۸���ʱ���Ⱥ�˳��洢�������У�
-//���������ù�Ʊһ�ο��ܻ�õ���������Ƕ��٣�
+//假设把某股票的价格按照时间先后顺序存储在数组中，
+//请问买卖该股票一次可能获得的最大利润是多少？
+
+#方法一
+从前往后遍历，动态更新最小买入点和最大收益
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0;
+        int nums_vec=prices.size();
+        if(nums_vec<2)
+        {
+            return maxProfit;
+        }
+        int buyprice=prices[0];
+        int max_current=0;
+        for(int i=1;i<nums_vec;i++)
+        {
+		#更新最小买入点
+                if(prices[i]<=buyprice)
+                {
+                    buyprice=prices[i];
+                }else
+                {
+		#更新最大收益
+                    max_current=prices[i]-buyprice;
+                    if(maxProfit<max_current)
+                    {
+                        maxProfit=max_current;
+                    }
+                }
+        }
+        return maxProfit;
+    }
+};
+
+#方法二
+
 #include"head.h"
 using namespace std;
 class Solution {
